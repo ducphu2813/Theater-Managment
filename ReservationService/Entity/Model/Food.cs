86fd0b7d3e8494a43dcc36754b783f79f4152ec1
ducphu2813 <taducphu2813.se@gmail.com>
2024-11-25 21:48:12 +0700

    add food image

--- ReservationService/Entity/Model/Food.cs
+++ ReservationService/Entity/Model/Food.cs
@@ -13,4 +13,5 @@ public class Food
     public string? FoodType { get; set; }
     public string? Description { get; set; }
     public int? Amount { get; set; }
+    public string? Image { get; set; }
 }
